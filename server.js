1990bd5f4d3cb4cdf33a9e7a1f864400587bb346
crazy2be <code@jmcgirr.com>
2017-11-26 12:21:04 -0500

    Remove almost all the global variables. Rename parse functions to make sense.

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ var app     = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
-var url        = "";
-var url_next   = "";
-var urls       = [];
-var sites      = [];
-var address    = [];
-var index_site = -1;
-var counter = 0;
 var DEFAULT_LAT = 43.461277;
 var DEFAULT_LON = -80.521149;
 
@@ -33,45 +25,34 @@ function cached_requester(url, cb) {
 
 var beaches = [];
 
-var first_page = function(url) {
+var search_page = function(url) {
 	console.log('url ======>', url);
 	if (url == '') return;
-	index_site = -1;
-	sites = [];
-	urls = [];
-	++counter;
-	console.log('counter =>', counter);
-	if (10 <= counter) return;
-	if (url === "") return;
-	cached_requester(url, function(error, response, html){
+	cached_requester(url, function(error, url, html) {
 		if (error) {
 			console.log("ERROR FIRST PAGE:", error);
 			return;
 		}
 		console.log('URL ===>', url);
 		var $ = cheerio.load(html);
-		urls = [].map.call($('a.title'), url => url.attribs.href);
-// 		console.log(Object.keys(urls).length, urls.length);
-// 		urls = $('a.title').slice();
-// 		console.log(Object.keys(urls).length, urls.length);
-// 		return
-		url_next = $(".pagination > a[title='Next']").attr('href');
-		url = " http://www.kijiji.ca"+url_next;
-		console.log('url_next ===>', url);
+		var href2url = href => "http://www.kijiji.ca" + href;
+		var urls = [].map.call($('a.title'), url => href2url(url.attribs.href));
+		var url_next = href2url($(".pagination > a[title='Next']").attr('href'));
+		console.log('url_next ===>', url_next);
 		console.log('urls =>', urls);
-		sites = sites.concat(urls);
-		test_next();
+		ad_page(urls, () => {
+			search_page(url_next);
+		});
 	});
 };
 
-var test_next = function() {
-	index_site = index_site + 1;
-	if (sites[index_site] === undefined) {
-		console.log("No site for index", index_site, sites);
+var ad_page = function(urls, cb) {
+	if (urls[0] === undefined) {
+		console.log("Onto the next page...")
+		cb();
 		return;
     }
-	var url_page = 'http://www.kijiji.ca'+sites[index_site];
-	cached_requester(url_page, function(error, url, html1){
+	cached_requester(urls[0], function(error, url, html1){
 		console.log('search page');
 		console.log('url_page =>', url);
 		if (error) {
@@ -91,7 +72,7 @@ var test_next = function() {
 		if (description.length < 5) {
 			console.log("[Empty ad]");
 			console.log("[This is probably a parse error]");
-			return test_next();
+			return ad_page(urls.slice(1), cb);
 		}
 
 		ad = {address: address, price: price, url: url, title: title,
@@ -99,7 +80,7 @@ var test_next = function() {
 		console.log('json ===>',ad);
 		beaches.push(ad);
 
-		test_next();
+		ad_page(urls.slice(1), cb);
 	});
 };
 
@@ -111,7 +92,7 @@ app.get('/maps', function(req, res, next){
 
 app.post('/add', urlencodedParser, function(req, res, next) {
 	counter = 0;
-	first_page(req, res, next);
+	search_page(req.body.url);
 	console.log('beaches =>', beaches);
 	res.render("index.ejs", { layout: false, lat: DEFAULT_LAT, lon: DEFAULT_LON, zoom:12, beaches:JSON.stringify(beaches)});
 	next();
@@ -125,4 +106,4 @@ app.listen('8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
 
-first_page("https://www.kijiji.ca/b-real-estate/kitchener-waterloo/c34l1700212?ll=43.461277,-80.521149");
+search_page("https://www.kijiji.ca/b-real-estate/kitchener-waterloo/c34l1700212?ll=43.461277,-80.521149");
